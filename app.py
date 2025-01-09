d198868e53cb5e16a2c5135b0d3baefbc0d4e57e
Angel Mares <chuisangel8@gmail.com>
2025-01-09 15:49:06 -0600

    Plantillas jinja básicas

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+from pymongo import MongoClient
+from os import getenv
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return render_template('base.jinja')
+
+@app.route('/all')
+def person():
+    with MongoClient(getenv('MONGOURI')) as mongo:
+        results = mongo.jornada122.staff.find({},{'_id':0}).to_list()
+
+    return render_template('person.jinja', results=results)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
