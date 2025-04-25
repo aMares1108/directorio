bdffbf4b5687c2392392e7736ffd692a4accb7b2
Angel Mares <chuisangel8@gmail.com>
2025-04-24 20:52:02 -0600

    Add personal view

--- app/blueprints/staff.py
+++ app/blueprints/staff.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, render_template
 from ..models.staff import Staff
+from ..models.extra import Extra
 
 user_bp = Blueprint('staff', __name__, url_prefix='/staff')
 
@@ -18,4 +19,9 @@ def person():
 
     return render_template('all.jinja', results=results)
 
-
+@user_bp.get('/<id>')
+def get_only_staff(id):
+    results = Staff.query.order_by(Staff.prioridad.desc(), Staff.nombre).all()
+    person = Staff.query.get_or_404(id)
+    extra = Extra.query.filter_by(staff_id=id)
+    return render_template('person.jinja', person=person, extra=extra, results=results)
--- app/models/extra.py
+++ app/models/extra.py
@@ -3,5 +3,5 @@ from ..extensions import db
 class Extra(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(64), nullable=False)
-    valor = db.Column(db.String(64), nullable=False)
-    staff_id = db.Column(db.Integer, db.ForeignKey('staff.id'))
+    valor = db.Column(db.Text, nullable=False)
+    staff_id = db.Column(db.Integer, db.ForeignKey('staff.id'), nullable=False)
